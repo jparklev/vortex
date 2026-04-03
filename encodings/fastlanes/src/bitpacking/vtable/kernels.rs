02ee0fe2a4f27c7caf7a708b01c45f465c04f010
Adam Gutglick <adam@spiraldb.com>
2026-04-03 17:34:24 +0100

    Try to implement compare/between for bitpacked arrays
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- encodings/fastlanes/src/bitpacking/vtable/kernels.rs
+++ encodings/fastlanes/src/bitpacking/vtable/kernels.rs
@@ -4,10 +4,14 @@
 use vortex_array::arrays::dict::TakeExecuteAdaptor;
 use vortex_array::arrays::filter::FilterExecuteAdaptor;
 use vortex_array::kernel::ParentKernelSet;
+use vortex_array::scalar_fn::fns::between::BetweenExecuteAdaptor;
+use vortex_array::scalar_fn::fns::binary::CompareExecuteAdaptor;
 
 use crate::BitPacked;
 
 pub(crate) const PARENT_KERNELS: ParentKernelSet<BitPacked> = ParentKernelSet::new(&[
+    ParentKernelSet::lift(&CompareExecuteAdaptor(BitPacked)),
+    ParentKernelSet::lift(&BetweenExecuteAdaptor(BitPacked)),
     ParentKernelSet::lift(&FilterExecuteAdaptor(BitPacked)),
     ParentKernelSet::lift(&TakeExecuteAdaptor(BitPacked)),
 ]);
