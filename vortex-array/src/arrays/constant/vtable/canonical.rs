2a66e39d83fde05979aacc428785924c5a3f40b4
Adam Gutglick <adam@spiraldb.com>
2026-05-12 18:29:26 +0100

    CR comments and better chunked variant handling
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- vortex-array/src/arrays/constant/vtable/canonical.rs
+++ vortex-array/src/arrays/constant/vtable/canonical.rs
@@ -24,6 +24,7 @@ use crate::arrays::NullArray;
 use crate::arrays::PrimitiveArray;
 use crate::arrays::StructArray;
 use crate::arrays::VarBinViewArray;
+use crate::arrays::VariantArray;
 use crate::arrays::varbinview::BinaryView;
 use crate::builders::builder_with_capacity;
 use crate::dtype::DType;
@@ -180,9 +181,7 @@ pub(crate) fn constant_canonicalize(
             Canonical::Extension(ExtensionArray::new(ext_dtype.clone(), storage_self))
         }
         DType::Variant(_) => {
-            unimplemented!(
-                "TODO(variant): canonicalization will use the child-array design in a follow-up"
-            )
+            Canonical::Variant(VariantArray::new(array.array().clone().into_array()))
         }
     })
 }
